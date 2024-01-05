e11ab629a6e5649f8381504167406b5557c628cc
Geryes-Doumit <geryes.doumet@gmail.com>
2024-01-05 22:01:42 +0100

    added server-specific settings

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,13 @@
-from ast import arg
-
-from interactions import Embed
 from bot_stuff import private_bot_data
 import discord
 from discord import app_commands
 from discord.ext import commands
 import responses
+import json
 
 async def send_message(message:discord.message, user_message:str):
     try:
-        response = responses.respond(user_message)
+        response = responses.respond(user_message, message.guild)
         if response != None:
             await message.reply(response)
     
@@ -24,21 +22,45 @@ def run_bot():
     
     bot = commands.Bot(command_prefix='/', intents=intents)
 
-    @bot.tree.command()
+    @bot.tree.command(description="List all the available features")
     async def features(interaction):
-        await interaction.response.send_message(embed=responses.features_command())
+        await interaction.response.send_message(embed=responses.features_command(interaction.guild))
+        
+    @app_commands.command(name="activate",description="Activate a feature")
+    @app_commands.choices(feature=[
+        discord.app_commands.Choice(name="di", value="di"),
+        discord.app_commands.Choice(name="cri", value="cri"),
+        discord.app_commands.Choice(name="backflip", value="backflip"),
+        discord.app_commands.Choice(name="insults", value="insults")
+    ])
+    async def activate(interaction, feature:str):
+        await interaction.response.send_message(embed=responses.activate_command(interaction.guild, feature))
+        
+    @app_commands.command(name="deactivate",description="Deactivate a feature")
+    @app_commands.choices(feature=[
+        discord.app_commands.Choice(name="di", value="di"),
+        discord.app_commands.Choice(name="cri", value="cri"),
+        discord.app_commands.Choice(name="backflip", value="backflip"),
+        discord.app_commands.Choice(name="insults", value="insults")
+    ])
+    async def deactivate(interaction, feature:str):
+        await interaction.response.send_message(embed=responses.deactivate_command(interaction.guild, feature))
     
-    @bot.tree.command()
+    bot.tree.add_command(activate)
+    bot.tree.add_command(deactivate)
+    
+    @bot.tree.command(description="Tell a joke")
     async def joke(interaction):
         await interaction.response.send_message(content=responses.joke_command())
     
     bot.tree.remove_command('help')
-    @bot.tree.command()
+    @bot.tree.command(description="help message")
     async def help(interaction):
         await interaction.response.send_message(embed=responses.help_command())
 
     @bot.event
     async def on_ready():
+        update_server_list(bot)
         print("Bot is ready 🔥🔥")
         
         try:
@@ -54,5 +76,27 @@ def run_bot():
         
         await send_message(message, message.content)
         await bot.process_commands(message)
+        
+    @bot.event
+    async def on_guild_join(guild):
+        update_server_list(bot)
                 
     bot.run(TOKEN)
+    
+def update_server_list(bot:commands.Bot):
+    filename = "src/bot_stuff/servers.json"
+    with open(filename, "r") as f:
+        server_list = json.load(f)
+    
+    for guild in bot.guilds:
+        if str(guild.id) not in server_list:
+            print(f"Adding {guild.name} to the server list")
+            server_list[str(guild.id)] = {}
+            server_list[str(guild.id)]["server_name"] = guild.name
+            server_list[str(guild.id)]["di"] = True
+            server_list[str(guild.id)]["cri"] = True
+            server_list[str(guild.id)]["backflip"] = True
+            server_list[str(guild.id)]["insults"] = True
+    
+    with open(filename, "w") as f:
+        json.dump(server_list, f, indent=4)
--- src/responses.py
+++ src/responses.py
@@ -1,55 +1,71 @@
 import random
-from sre_constants import IN
 import discord
 import requests
-import data
+import responses_data
+import json
 
-def respond(message:str):
+def respond(message:str, guild):
     lower_message = message.lower()
     
-    if "backflip" in lower_message or "back flip" in lower_message:
-        return data.backflip_gifs[random.randint(0, len(data.backflip_gifs) - 1)]
+    with open("src/bot_stuff/servers.json", "r") as f:
+        server_list = json.load(f)
+    
+    if feature_activated(server_list, guild, "backflip"):
+        if "backflip" in lower_message or "back flip" in lower_message:
+            return responses_data.backflip_gifs[random.randint(0, len(responses_data.backflip_gifs) - 1)]
     
     for word in lower_message.split():
-        if word in data.french_insults:
-            return data.fr_responses_to_insults[random.randint(0, len(data.fr_responses_to_insults) - 1)]
+        if word in responses_data.french_insults and feature_activated(server_list, guild, "insults"):
+            return responses_data.fr_responses_to_insults[random.randint(0, len(responses_data.fr_responses_to_insults) - 1)]
         
         if word.__len__() < 5:
             continue
-        if word.startswith("di") or word.startswith("dy"):
+        if (word.startswith("di") or word.startswith("dy")) and feature_activated(server_list, guild, "di"):
             return alpha_characters_of(word[2:])
-        if word.startswith("cri") or word.startswith("cry"):
+        if (word.startswith("cri") or word.startswith("cry")) and feature_activated(server_list, guild, "cri"):
             return alpha_characters_of(word[3:]).upper()
 
 def alpha_characters_of(word:str) -> str:
     return ''.join([i for i in word if i.isalpha()])
+
+def feature_activated(server_list, guild, feature:str) -> bool:
+    return server_list[str(guild.id)][feature]
 
 def joke_command():
     response = requests.get("https://official-joke-api.appspot.com/random_joke")
     json = response.json()
     return json["setup"] + "\n" + json["punchline"]
 
-def features_command() -> discord.Embed:
+def features_command(guild) -> discord.Embed:
+    with open("src/bot_stuff/servers.json", "r") as f:
+        server_list = json.load(f)
+    
     title =  "Available features"
     embed = discord.Embed(title=title, color=discord.Color.yellow())
     
-    title = "Backflip GIFs:" 
+    title = "Backflip GIFs:" + status_string(server_list[str(guild.id)]["backflip"])
     desc = "When a message contains 'backflip', the bot will respond with a backflip GIF"
     embed.add_field(name=title, value=desc, inline=False)
     
-    title = "Di:"
+    title = "Di:" + status_string(server_list[str(guild.id)]["di"])
     desc = "If a word starts with 'di' or 'dy', the bot will answer with the rest of the word"
     embed.add_field(name=title, value=desc, inline=False)
     
-    title = "Cri:"
+    title = "Cri:" + status_string(server_list[str(guild.id)]["cri"])
     desc = "If a word starts with 'cri' or 'cry', the bot will answer with the rest of the word in uppercase"
     embed.add_field(name=title, value=desc, inline=False)
     
-    title = "Insults:"
-    desc = "If a message contains a french insult, the bot will answer with a random response"
+    title = "Insults:" + status_string(server_list[str(guild.id)]["insults"])
+    desc += "If a message contains a french insult, the bot will answer with a random response"
     embed.add_field(name=title, value=desc, inline=False)
     
+    footer = "To activate or deactivate a feature, use the /activate or /deactivate commands"
+    embed.set_footer(text=footer)
+    
     return embed
+
+def status_string(bool):
+    return "" if bool else "(deactivated)"
 
 def help_command() -> discord.Embed:
     title =  "Available commands"
@@ -63,8 +79,48 @@ def help_command() -> discord.Embed:
     desc = "The bot will answer with a list of all the available features (automatic responses)"
     embed.add_field(name=title, value=desc, inline=False)
     
+    title = "/activate or /deactivate [feature]:"
+    desc = "Activate or deactivate a feature"
+    embed.add_field(name=title, value=desc, inline=False)
+    
     title = "/help:"
     desc = "Shows this message"
     embed.add_field(name=title, value=desc, inline=False)
     
+    return embed
+
+def activate_command(guild, feature:str):
+    with open("src/bot_stuff/servers.json", "r") as f:
+        server_list = json.load(f)
+    
+    if server_list[str(guild.id)][feature] == True:
+        embed = discord.Embed(color=discord.Color.red())
+        embed.add_field(name="Feature already activated", value=f"The '{feature}' feature is already activated for this server", inline=False)
+        return embed
+    
+    server_list[str(guild.id)][feature] = True
+    
+    with open("src/bot_stuff/servers.json", "w") as f:
+        json.dump(server_list, f, indent=4)
+        
+    embed = discord.Embed(color=discord.Color.green())
+    embed.add_field(name="Feature activated", value=f"The '{feature}' feature has been activated for this server", inline=False)
+    return embed
+
+def deactivate_command(guild, feature:str):
+    with open("src/bot_stuff/servers.json", "r") as f:
+        server_list = json.load(f)
+    
+    if server_list[str(guild.id)][feature] == False:
+        embed = discord.Embed(color=discord.Color.red())
+        embed.add_field(name="Feature already deactivated", value=f"The '{feature}' feature is already deactivated for this server", inline=False)
+        return embed
+    
+    server_list[str(guild.id)][feature] = False
+    
+    with open("src/bot_stuff/servers.json", "w") as f:
+        json.dump(server_list, f, indent=4)
+    
+    embed = discord.Embed(color=discord.Color.yellow())
+    embed.add_field(name="Feature deactivated", value=f"The '{feature}' feature has been deactivated for this server", inline=False)
     return embed
